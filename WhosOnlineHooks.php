920b2612ec4f43ed6b8309aeaaa33555731b66fc
Jack Phoenix <jack@countervandalism.net>
2017-07-23 19:20:03 +0300

    Don't write to the DB if the DB is read-only
    Also swapped one global to its RequestContext equivalent.
    
    Change-Id: I80694e36db7e88eb7d1a80215328ce248755febb

--- WhosOnlineHooks.php
+++ WhosOnlineHooks.php
@@ -5,17 +5,21 @@
 class WhosOnlineHooks {
 
 	// update online data
-	public static function onBeforePageDisplay() {
-		global $wgUser;
+	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
+		// don't write to the DB if the DB is read-only
+		if ( wfReadOnly() ) {
+			return true;
+		}
 
 		// write to DB (use master)
 		$dbw = wfGetDB( DB_MASTER );
 		$now = gmdate( 'YmdHis', time() );
 
+		$user = $out->getUser();
 		// row to insert to table
 		$row = array(
-			'userid' => $wgUser->getId(),
-			'username' => $wgUser->getName(),
+			'userid' => $user->getId(),
+			'username' => $user->getName(),
 			'timestamp' => $now
 		);
 
